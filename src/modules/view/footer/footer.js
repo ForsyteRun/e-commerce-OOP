246f36c96f8e5e033d5485520197c3ebfbd8b1fe
ForsyteRun <forsyterun@gmail.com>
2023-06-01 08:20:21 +0300

    feat: add types inside .ts

--- src/modules/view/footer/footer.js
+++ src/modules/view/footer/footer.js
@@ -1,2 +1,16 @@
-"use strict";
-console.log("footer");
+var CSSClasses = {
+    FOOTER: "footer",
+};
+var TEXT = "Good morning Footer!";
+var FooterView = (function () {
+    function FooterView() {
+    }
+    FooterView.prototype.createView = function () {
+        var element = document.createElement("footer");
+        element.textContent = TEXT;
+        element.classList.add(CSSClasses.FOOTER);
+        return element;
+    };
+    return FooterView;
+}());
+export default FooterView;
--- src/modules/view/footer/footer.ts
+++ src/modules/view/footer/footer.ts
@@ -1,1 +1,21 @@
-console.log("footer");
+export interface CSSClassesType {
+  FOOTER: "footer";
+}
+
+const CSSClasses: CSSClassesType = {
+  FOOTER: "footer",
+};
+
+const TEXT = "Good morning Footer!";
+
+export default class FooterView {
+  constructor() {}
+
+  createView(): HTMLElement {
+    const element: HTMLElement = document.createElement("footer");
+
+    element.textContent = TEXT;
+    element.classList.add(CSSClasses.FOOTER);
+    return element;
+  }
+}
